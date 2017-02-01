bf972697ba78a28c1128bfd6702a0c942654b7d9
varun-bhatt <varun.bhatt@marutitech.com>
2017-02-01 19:13:25 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace StudentInfo
+{
+    [DataContract]
+    public class Student1
+    {
+       
+        [DataMember]
+        public int StudentId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string StudentName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Number
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string StudentCity
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string StudentSurname
+        {
+            get;
+            set;
+        }
+
+    }
+   
+
+}
